1e00bd8d27664df5946b40a498aa1999a3d3d17c
ilkay Polat <57589698+ilkaypolat1985@users.noreply.github.com>
2020-04-11 13:47:23 +0200

    Created a singleton class by using Enum

---
+++ src/com/designpattern/creational/singleton/SingletonV2.java
@@ -0,0 +1,9 @@
+package com.designpattern.creational.singleton;
+
+public enum SingletonV2 {
+    INSTANCE;
+    
+    public void doSomething(){
+      System.out.println("Doing something...");
+    }
+}
